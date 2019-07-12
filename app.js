715def10c089763fe93577cc049ce645c1d8e7ca
Raphael Bastos <raphael@dep.fem.unicamp.br>
2019-07-12 10:19:53 -0300

    https

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require('path');
 // const nomeApp = process.env.npm_package_name;
 const nomeApp = "ui-sistema-treinamentos";
 const app = express();
- 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`);
+        else
+            next()
+    })
+}
+
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
- 
+
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
- 
+
 app.listen(process.env.PORT || 8080);
